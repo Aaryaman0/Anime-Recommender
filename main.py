19fbf9d5cef622dc9a8e6cdc9f9ea7bf7b7f994e
Aaryaman0 <abhageri@uwaterloo.ca>
2022-07-18 19:13:49 -0400

    Started MAL web scraper

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # anime recommnder time
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://myanimelist.net/animelist/Aaryaman0').text
+print(html_text)
+
+soup = BeautifulSoup(html_text, 'lxml')
