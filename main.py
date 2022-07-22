7328380309dc5290caf8c936c7e4ebc5e6f713f2
Aaryaman0 <abhageri@uwaterloo.ca>
2022-07-21 23:58:41 -0400

    Updates to Scraper

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get('https://myanimelist.net/animelist/Aaryaman0').text
-print(html_text)
+# print(html_text)
 
 soup = BeautifulSoup(html_text, 'lxml')
+anime = soup.find_all('tbody', class_ = "list-item")
+for item in anime:
+    anime_name = item.text
+    print(anime_name)
